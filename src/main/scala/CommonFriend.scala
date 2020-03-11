0c53a27473fec092bac0dcb1afc884cf38cd73e3
Conrad <36934379+Conradyen@users.noreply.github.com>
2020-03-11 00:00:42 -0500

    Update CommonFriend.scala

--- src/main/scala/CommonFriend.scala
+++ src/main/scala/CommonFriend.scala
@@ -29,7 +29,8 @@ object CommonFriend {
     }) )
 
     /* reduce */
-    var reduce = map.map(x=>((x._1._1, x._1._2), x._2.split(",").toSet)).reduceByKey((a,b)=>a.intersect(b)).sortByKey().map(x=>(x._1 + "\t"+x._2.size))
+    var reduce = map.map(x=>((x._1._1, x._1._2), x._2.split(",").toSet))
+    .reduceByKey((a,b)=>a.intersect(b)).sortByKey().map(x=>(x._1 + "\t"+x._2.size))
 
     /* or save the output to file */
     reduce.saveAsTextFile("Q1out.txt")
@@ -53,14 +54,17 @@ object CommonFriend {
     } )
 
     /* reduce */
-    var commonreduce = commonmap.map(x=>((x._1._1, x._1._2), x._2.split(",").toSet)).reduceByKey((a,b)=>a.intersect(b)).sortByKey().map(x=>(x._1._1,x._1._2,x._2.size))
+    var commonreduce = commonmap.map(x=>((x._1._1, x._1._2), x._2.split(",").toSet))
+    .reduceByKey((a,b)=>a.intersect(b)).sortByKey().map(x=>(x._1._1,x._1._2,x._2.size))
 
     var data_map = user_data_input.map(line=>line.split(",")).map(x=>(x(0),(x(1),x(2),x(3)))).collect.toMap
 
     var friend_map = commonreduce.map(line=>(line._3,data_map.get(line._1).get,data_map.get(line._2).get)).sortBy(-_._1)
 
 //    <Total number of Common Friends><TAB><First Name of User A><TAB><Last Name of User A> <TAB><address of User A><TAB><First Name of User B><TAB><Last Name of User B><TAB><address of User B>
-    var friend_reduce = sc.parallelize(friend_map.map(x=>(x._1+"\t"+x._2._1+"\t"+x._2._2+"\t"+x._2._3+"\t"+x._3._1+"\t"+x._3._2+"\t"+x._3._3)).take(10))
+    var friend_reduce = sc.parallelize(friend_map
+                                       .map(x=>(x._1+"\t"+x._2._1+"\t"+x._2._2+"\t"+x._2._3+"\t"+x._3._1+"\t"+x._3._2+"\t"+x._3._3))
+                                       .take(10))
 
     friend_reduce.saveAsTextFile("Q2out.txt")
 
@@ -80,8 +84,11 @@ object CommonFriend {
     var bussiness_map = bussiness_input.map(line=>line.split("::")).map(x=>(x(0),x(1))).collect.toMap
   //.filter(line=>regex.findFirstIn(line(1)).isDefined)
 
-    var review_map = review_input.map(line=>line.split("::")).map(x=>(x(0),x(1),bussiness_map.get(x(2)).get,x(3))).filter(line=>regex.findFirstIn(line._3).isDefined).map(x=>(x._2,x._4))
-    var reduce = review_map.map{case(a,b)=> (a,new StrCollector(b))}.reduceByKey( _ combine _ ).map{case(a,b)=>List(a,b.out).mkString("::")}
+    var review_map = review_input.map(line=>line.split("::"))
+    .map(x=>(x(0),x(1),bussiness_map.get(x(2)).get,x(3))).filter(line=>regex.findFirstIn(line._3).isDefined)
+    .map(x=>(x._2,x._4))
+    var reduce = review_map.map{case(a,b)=> (a,new StrCollector(b))}.reduceByKey( _ combine _ )
+    .map{case(a,b)=>List(a,b.out).mkString("::")}
 
     reduce.saveAsTextFile("Q3out.txt")
 
@@ -100,7 +107,8 @@ object CommonFriend {
 
     //hash map
     var review_map = review_input.map(line=>line.split("::")).map(x=>(x(2),x(3).toDouble))
-    var review_reduce = sc.parallelize(review_map.map{case(a,b)=> (a,new AvgCollector(b)) }.reduceByKey( _ combine _ ).map{case(x,y)=>(x,bussiness_map.get(x).get._1,bussiness_map.get(x).get._2,y.avg)}
+    var review_reduce = sc.parallelize(review_map.map{case(a,b)=> (a,new AvgCollector(b)) }
+                                       .reduceByKey( _ combine _ ).map{case(x,y)=>(x,bussiness_map.get(x).get._1,bussiness_map.get(x).get._2,y.avg)}
     .sortBy(-_._4).take(10).map(x=>List(x._1,x._2,x._3,x._4).mkString("::")))
 
     review_reduce.saveAsTextFile("Q4out.txt")
